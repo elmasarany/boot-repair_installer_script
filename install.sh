b2849e94389d6036e08863df27ee6ad1574d5782
Ahmed Elmasarany <a.elmasarany@gmail.com>
2017-10-25 22:21:49 +0200

    change the condition of root

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Make sure only root can run our script
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   echo "Try running: sudo $0" 1>&2
-   exit 1
+if [ "$USER" = 'root' ]
+    then
+        gdialog --title "User Check" --msgbox "User is root, you can execute the script successfully." 200 150        
+        echo "User is root, you can execute the script successfully"
+    else
+        gdialog --title "User Check" --msgbox "User is not Root. Please run the script as root user." 200 150
+        echo "User is not Root. Please run the script as root user. try: sudo $0 "
+        exit 1
 fi
 
 (dpkg -i python-gi* &&
